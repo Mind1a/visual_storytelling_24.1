f02ab943e6ee9f8653e8960e699b47b9937b70f9
luka_C.B <lukaaslamazashvili20@gmail.com>
2024-06-13 16:07:56 +0400

    Adding landing page

---
+++ src/views/landing/Landing.jsx
@@ -0,0 +1,13 @@
+import { Video } from "../../components/video";
+
+const Landing = () => {
+  return (
+    <main className="landing-container">
+      <h1 className="title">ვიზუალური თხრობა</h1>
+      <Video />
+      <button className="landing-btn">გაგრძელება</button>
+    </main>
+  );
+};
+
+export default Landing;
