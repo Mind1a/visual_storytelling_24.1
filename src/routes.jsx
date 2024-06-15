e172938f40b4f1853893f7fe33d86c3d750a4b57
Teona Zavrashvili <teona.zavrashvili@gmail.com>
2024-06-15 07:05:04 +0400

    add navigation and update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,19 +1,48 @@
-import { Route, Routes } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import { Layout } from "./components/layout";
 import { Landing } from "./views/landing";
+import { Home } from "./views/home";
+import { Gallery } from "./views/gallery";
+import { Guide } from "./views/guide";
+import { About } from "./views/about";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Layout />}>
+      <Route
+        index
+        element={<Landing />}
+      />
+      <Route
+        path="home"
+        element={<Home />}
+      />
+      <Route
+        path="gallery"
+        element={<Gallery />}
+      />
+      <Route
+        path="guide"
+        element={<Guide />}
+      />
+      <Route
+        path="about"
+        element={<About />}
+      />
+    </Route>,
+  ),
+);
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={<Layout />}>
-        <Route
-          index
-          element={<Landing />}
-        />
-      </Route>
-    </Routes>
+    <RouterProvider router={router} />
   );
 };
 
