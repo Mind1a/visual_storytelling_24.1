92572769199e17bd0d28691f78d903a0ae8162b5
Teona Zavrashvili <teona.zavrashvili@gmail.com>
2024-06-15 08:18:35 +0400

    update Layout component styles

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,10 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { Header } from "../header";
+import styles from "./Layout.module.scss";
 
 const Layout = () => {
+  const location = useLocation();
+  const isLandingPage = location.pathname === "/";
+  const bgClass = isLandingPage ? styles.landingBg : styles.defaultBg;
+
   return (
-    <main className="layout">
-      <Outlet />
-    </main>
+    <div className={`${styles.layout} ${bgClass}`}>
+      {!isLandingPage && <Header />}
+      <main
+        className={`${styles.main} ${!isLandingPage ? styles.mainDefault : ""}`}>
+        <Outlet />
+      </main>
+    </div>
   );
 };
 
