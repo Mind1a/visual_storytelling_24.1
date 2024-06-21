18f4d64808872cde84d797db792297e00327d9f5
luka_C.B <lukaaslamazashvili20@gmail.com>
2024-06-21 14:45:50 +0400

    Added gallery data and gallery cards

---
+++ src/components/gallery/Card.jsx
@@ -0,0 +1,24 @@
+import styles from "./Card.module.scss";
+import dlIcon from "../../assets/images/dl-icon.png";
+
+const Card = ({ item }) => {
+  return (
+    <div className={styles.card}>
+      <div className={item.type === "verb" ? styles.verbBox : styles.box}>
+        <img
+          src={item.image}
+          alt={item.word}
+          className={styles.image}
+        />
+        <img
+          src={dlIcon}
+          alt="download icon"
+          className={styles.dlIcon}
+        />
+      </div>
+      <p className={styles.word}>{item.word}</p>
+    </div>
+  );
+};
+
+export default Card;
--- src/views/gallery/Gallery.jsx
+++ src/views/gallery/Gallery.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import styles from "./Gallery.module.scss";
+import { Card } from "../../components/gallery";
+import { galleryData } from "../../data";
 
 const Gallery = () => {
-  return <div>Gallery</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.filter}>
+        {" "}
+        <h1>here goes filters</h1>
+      </div>
+      <div className={styles.gallery}>
+        {galleryData.slice(0, 8).map(item => (
+          <Card
+            key={item.id}
+            item={item}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Gallery;
