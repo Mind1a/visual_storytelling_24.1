3cd74d62d4bb82c636afed4d56b3f538f88ea498
TeonaTsiramua <tsiramuateona@gmail.com>
2024-07-04 04:30:29 +0400

    Refactor data and add sentence data for categories

---
+++ src/data/sentence/family.js
@@ -0,0 +1,68 @@
+import { family } from "../index.js";
+import { createSentenceData } from "../../utils";
+
+export const familyData = [
+  [
+    // 1st sentence
+    createSentenceData(
+      family,
+      ["ოჯახ", "ოჯახ"],
+      ["ჭამს", "იცვამს"],
+      ["ში", "ზე"],
+    ),
+  ],
+  [
+    // 2nd sentence
+    createSentenceData(
+      family,
+      ["ოჯახ", "ოჯახ"],
+      ["ჭამს", "იცვამს"],
+      ["ში", "ზე"],
+    ),
+  ],
+  [
+    // 3rd sentence
+    createSentenceData(
+      family,
+      ["ოჯახ", "ოჯახ"],
+      ["ჭამს", "იცვამს"],
+      ["ში", "ზე"],
+    ),
+  ],
+  [
+    // 4th sentence
+    createSentenceData(
+      family,
+      ["ოჯახ", "ოჯახ"],
+      ["ჭამს", "იცვამს"],
+      ["ში", "ზე"],
+    ),
+  ],
+  [
+    // 5th sentence
+    createSentenceData(
+      family,
+      ["ოჯახ", "ოჯახ"],
+      ["ჭამს", "იცვამს"],
+      ["ში", "ზე"],
+    ),
+  ],
+  [
+    // 6th sentence
+    createSentenceData(
+      family,
+      ["ოჯახ", "ოჯახ"],
+      ["ჭამს", "იცვამს"],
+      ["ში", "ზე"],
+    ),
+  ],
+  [
+    // 7th sentence
+    createSentenceData(
+      family,
+      ["ოჯახ", "ოჯახ"],
+      ["ჭამს", "იცვამს"],
+      ["ში", "ზე"],
+    ),
+  ],
+];
---
+++ src/data/sentence/objects.js
@@ -0,0 +1,68 @@
+import { objects } from "../index.js";
+import { createSentenceData } from "../../utils";
+
+export const objectsData = [
+  [
+    // 1st sentence
+    createSentenceData(
+      objects,
+      ["საგან", "საგან"],
+      ["ჭამს", "იცვამს"],
+      ["ში", "ზე"],
+    ),
+  ],
+  [
+    // 2nd sentence
+    createSentenceData(
+      objects,
+      ["საგან", "საგან"],
+      ["ჭამს", "იცვამს"],
+      ["ში", "ზე"],
+    ),
+  ],
+  [
+    // 3rd sentence
+    createSentenceData(
+      objects,
+      ["საგან", "საგან"],
+      ["ჭამს", "იცვამს"],
+      ["ში", "ზე"],
+    ),
+  ],
+  [
+    // 4th sentence
+    createSentenceData(
+      objects,
+      ["საგან", "საგან"],
+      ["ჭამს", "იცვამს"],
+      ["ში", "ზე"],
+    ),
+  ],
+  [
+    // 5th sentence
+    createSentenceData(
+      objects,
+      ["საგან", "საგან"],
+      ["ჭამს", "იცვამს"],
+      ["ში", "ზე"],
+    ),
+  ],
+  [
+    // 6th sentence
+    createSentenceData(
+      objects,
+      ["საგან", "საგან"],
+      ["ჭამს", "იცვამს"],
+      ["ში", "ზე"],
+    ),
+  ],
+  [
+    // 7th sentence
+    createSentenceData(
+      objects,
+      ["საგან", "საგან"],
+      ["ჭამს", "იცვამს"],
+      ["ში", "ზე"],
+    ),
+  ],
+];
